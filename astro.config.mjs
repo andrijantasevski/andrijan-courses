2494a8aa04d2fc869c715d469cd50a59533cea7e
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-03-22 17:55:20 +0100

    Test for SSR on Cloudflare.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind(), react(), mdx()],
+  adapter: cloudflare()
 });
