22d874788d60179d35a59a52e81b1c2f2f51d0e1
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-03-26 16:30:29 +0200

    Test to return random secret.

--- src/pages/courses/index.ts
+++ src/pages/courses/index.ts
@@ -8,42 +8,44 @@ import { getRuntime } from "@astrojs/cloudflare/runtime";
 
 interface Env {
   API_SECRET_KEY: string;
+  RANDOM_SECRET: string;
   ENVIRONMENT: string;
 }
 
 export const get: APIRoute = async ({ request }) => {
   const runtime = getRuntime<Env>(request);
 
-  errorResponse({
-    errorMessage: `This is mode ${runtime.env.ENVIRONMENT}`,
-    statusCode: 404,
-  });
+  return new Response(
+    JSON.stringify({ message: `RANDOM_SECRET: ${runtime.env.RANDOM_SECRET}` }),
+    {}
+  );
 
-  const apiSecretKey = request.headers.get("X-API-KEY");
+  // const apiSecretKey = request.headers.get("X-API-KEY");
+  // ``;
 
-  if (
-    !apiSecretKey ||
-    !timingSafeEqual(apiSecretKey, runtime.env.API_SECRET_KEY)
-  ) {
-    return errorResponse({
-      statusCode: 403,
-      errorMessage: "Unauthorized access.",
-    });
-  }
+  // if (
+  //   !apiSecretKey ||
+  //   !timingSafeEqual(apiSecretKey, runtime.env.API_SECRET_KEY)
+  // ) {
+  //   return errorResponse({
+  //     statusCode: 403,
+  //     errorMessage: "Unauthorized access.",
+  //   });
+  // }
 
-  const coursesCollection = await getCollection("courses");
+  // const coursesCollection = await getCollection("courses");
 
-  if (coursesCollection.length === 0) {
-    return errorResponse({
-      statusCode: 404,
-      errorMessage: "Content not found.",
-    });
-  }
+  // if (coursesCollection.length === 0) {
+  //   return errorResponse({
+  //     statusCode: 404,
+  //     errorMessage: "Content not found.",
+  //   });
+  // }
 
-  const courses = coursesCollection.map(transformCourse);
+  // const courses = coursesCollection.map(transformCourse);
 
-  return new Response(JSON.stringify(courses), {
-    status: 200,
-    statusText: "Success",
-  });
+  // return new Response(JSON.stringify(courses), {
+  //   status: 200,
+  //   statusText: "Success",
+  // });
 };
